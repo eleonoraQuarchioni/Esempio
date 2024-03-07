febb9dcdf1bde5eaf03cad8bf4795bff42c2de98
eleonoraQuarchioni <arymary922@gmail.com>
2024-03-07 17:34:13 +0100

    Initial commit

---
+++ src/main/java/com/example/ProgettoGit/ProgettoGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProgettoGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProgettoGit/ProgettoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProgettoGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
